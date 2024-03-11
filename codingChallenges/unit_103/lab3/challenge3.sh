838a2162e206af212062c6dda8c4266568fff7f4
jack <justusworking245@gmail.com>
2024-03-11 18:45:07 -0500

    coding challenge3

--- codingChallenges/unit_103/lab3/challenge3.sh
+++ codingChallenges/unit_103/lab3/challenge3.sh
@@ -3,8 +3,11 @@
 # This ops challenges is about conditinal stamtment of if else statments and how they work
 # We are going to take a varibale with the number and have the computer tell us if its greater than 5 less than 5 or equals 5
 
-number=4
+number=5
 
-if(number < 5){
-    
-} 
+if (($number < 5)) 
+    then echo "this is less than 5"
+elif (($number > 5))
+    then echo " this number is a Lot greater than 5"
+else echo "idk this number is probably 5"
+fi
